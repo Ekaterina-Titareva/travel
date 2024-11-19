104147bb04a5bc0ca872e0ec627e01f06e16e2bb
Ekaterina-Titareva <ekaterina.titareva.ca@gmail.com>
2024-11-19 23:44:02 +0300

    gh-pages

--- src/utils/getCurrentTime.ts
+++
@@ -1,8 +0,0 @@
-const getCurrentTime = (): string => {
-  const now = new Date();
-  const hours = now.getHours().toString().padStart(2, "0");
-  const minutes = now.getMinutes().toString().padStart(2, "0");
-  return `${hours}:${minutes}`;
-};
-
-export default getCurrentTime;
